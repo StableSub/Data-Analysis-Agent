5db9c3bee1bb9c0cec53d89efcac9cba0d60ebe5
fkrdud1125 <fkrdud1125@naver.com>
2026-02-14 15:57:42 +0900

    delete PreprocessBackend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Preprocess from './pages/Preprocess';
-import PreprocessBackend from './pages/PreprocessBackend';
 import { AppLayout } from './components/layout/AppLayout';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
           <Route path="/" element={<Navigate to="/chat" replace />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/preprocess" element={<Preprocess />} />
-          <Route path="/preprocess/backend" element={<PreprocessBackend />} />
           {/* Removed routes: /datasets, /datasets/meta, /export */}
         </Route>
       </Routes>
